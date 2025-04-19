9980c5d458f5cbf4e3c8596cfd9c5f2e5942556e
harichandra-github <harichandrachaudhari21@gmail.com>
2025-04-19 16:33:03 +0530

    added code for launch browser

---
+++ src/main/java/com/qa/opencart/factory/DriverFactory.java
@@ -0,0 +1,79 @@
+
+package com.qa.opencart.factory;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import com.qa.opencart.constants.AppConstants;
+import com.qa.opencart.exceptions.BrowserException;
+
+public class DriverFactory {
+
+	WebDriver driver;
+	Properties prop;
+
+	/**
+	 * This method is used to initialize the driver on the basis of given browser name
+	 * 
+	 * @param browserName
+	 */
+	public WebDriver initDriver(Properties prop) {
+		
+		String browserName = prop.getProperty("browser");
+		
+		System.out.println("browser name : " + browserName);
+
+		switch (browserName.toLowerCase().trim()) {
+		case "chrome":
+			driver = new ChromeDriver();
+			break;
+		case "edge":
+			driver = new EdgeDriver();
+			break;
+		case "firefox":
+			driver = new FirefoxDriver();
+			break;
+		case "safari":
+			driver = new SafariDriver();
+			break;
+
+		default:
+			System.out.println("plz pass the valid browser name..." + browserName);
+			throw new BrowserException("===INVALID BROWSER===");
+		}
+
+		driver.get(prop.getProperty("url"));//login page url
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		return driver;
+	}
+	
+	/**
+	 * this is used to initialize the configuration properties
+	 * @return
+	 */
+	public Properties initProp() {
+		prop = new Properties();
+		try {
+			FileInputStream ip = new FileInputStream(AppConstants.configFile);
+			prop.load(ip);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return prop;
+	}
+	
+
+}
+
