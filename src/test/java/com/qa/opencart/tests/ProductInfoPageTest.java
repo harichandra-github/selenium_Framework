ad2789b5dfcf38b8f80a7370b5fc46a7950be586
harichandra-github <harichandrachaudhari21@gmail.com>
2025-05-11 10:48:47 +0530

    Added a search and product info tests

--- src/test/java/com/qa/opencart/tests/ProductInfoPageTest.java
+++ src/test/java/com/qa/opencart/tests/ProductInfoPageTest.java
@@ -1,5 +1,37 @@
 package com.qa.opencart.tests;
 
-public class ProductInfoPageTest {
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.qa.opencart.base.BaseTest;
+
+public class ProductInfoPageTest extends BaseTest{
+	
+	
+	@BeforeClass
+	public void productInfoSetup() {
+		accPage = loginPage.doLogin(prop.getProperty("username"), prop.getProperty("password"));
+	}
+
+	//AAA --- Arrange Act Assert(1)
+	@Test
+	public void productHeaderTest() {
+		searchResultsPage =  accPage.doSearch("macbook");
+		productInfoPage = searchResultsPage.selectProduct("MacBook Pro");
+		String actHeader = productInfoPage.getProductHeader();
+		Assert.assertEquals(actHeader, "MacBook Pro");
+	}
+	
+	@Test
+	public void productImageCountTest() {
+		searchResultsPage =  accPage.doSearch("macbook");
+		productInfoPage = searchResultsPage.selectProduct("MacBook Pro");
+		int actImageCount = productInfoPage.getProductImagesCount();
+		Assert.assertEquals(actImageCount, 3);
+	}
+	
+	
+	
 
 }
